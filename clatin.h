554f5a251ce8d0030a1b4eac87cad6b076b008a9
p33r-positron <84068153+p33r-positron@users.noreply.github.com>
2021-11-13 20:12:35 +0100

    Update clatin.h

--- clatin.h
+++ clatin.h
@@ -1,24 +1,84 @@
-#ifndef CLATIN_H
-#define CLATIN_H
+#ifndef FRANC_H
+#define FRANC_H
 
-//les types
-typedef int numerus;
-typedef short breve;
-typedef float supernatet;
-typedef char littera;
-//typedef long log;
-#define praesagio auto 
+#define positif unsigned
+typedef void vide;
+typedef void rien;
 
-//les fonctions
-#define magnitudine sizeof
-#define aditus main
-#define remito return
-#define scribere printf
+typedef char mini;
+typedef short petit;
+typedef int entier;
+typedef long grand;
+typedef long long très grand
 
-//structures conditionnelles
-#define relinquo break
-#define dum while
+typedef char caractere;
+
+typedef char* texte;
+
+typedef float virgule;
+typedef double decimal;
+
+#define FICHIER FILE
+
+#define taillede sizeof
+#define principal main
+#define retournes return
+#define retourne return
+#define renvoies return
+#define renvoie return
+
+#define mets puts
+#define met puts
+#define écrisf printf
+
+#define ouvre fopen
+#define ouvres fopen
+
+#define fprendc fgetc
+#define fprends fgets
+#define flis fread
+#define fécris fwrite
+#define fmet fputs
+#define fmets fputs
+#define fécrisf fprintf
+
+#define exécute system
+#define exécutes system
+#define emprunte malloc
+#define empruntes malloc
+#define roule_dés srand
+#define roules_dés srand
+#define roule_dé srand
+#define roules_dé srand
+#define aléatoire rand
+#define hasard rand
+
+#define arrêtes break
+#define arrête break
+#define tantque while
+#define tant while
+#define fais do
 #define si if 
+#define sinon else
+#define encore else if
+#define teste switch
+#define testes switch
+#define cas case
+#define aupire default
+#define pour for
+
+#define vrai 1
+#define false 0
+#define NUL 0
+
+#define non !
+#define et &&
+#define ou ||
+
+#define et_l &
+#define ou_l |
+#define non_l ~
+#define soit ^
 
 //autre
 
